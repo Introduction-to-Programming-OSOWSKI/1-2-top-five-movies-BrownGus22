33c2ff818884a83b03c0b881b3d75b20d1c5d48a
BrownGus22 <815788@mystma.org>
2021-12-15 08:56:46 -0600

    Three

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
 #WRITE YOUR CODE IN THIS FILE
 def topFiveMovies(a,b,c,d,e):
-    a = input()
-    b = input()
-    c = input()
-    d = input()
-    e = input()
-    return a, b, c, d, e
+    return topFiveMovies(a, b, c, d, e)
